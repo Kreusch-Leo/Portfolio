c6c847a092de3964c0888565f4bd9e65ff41fddc
Leonardo Kreusch <kreuschleoanc@gmail.com>
2026-01-27 16:18:19 -0300

    [ADD] plaholder pages

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+const Contacts = () => {
+  return (
+    <div className="page">
+      <h1>Contacts Page</h1>
+      <p>This is the Contacts page content.</p>
+    </div>
+  )
+}       
+
+export default Contacts
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+  return (
+    <div className="page">
+      <h1>Projects Page</h1>
+      <p>This is the Projects page content.</p>
+    </div>
+  )
+}
+
+export default Projects
