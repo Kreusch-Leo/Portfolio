2d7b2bab9b1ff7045d58515ecec2032bcdce9480
Leonardo Kreusch <kreuschleoanc@gmail.com>
2026-01-27 01:21:59 -0300

    [FIX] unused comps

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import WhoAmI from './pages/WhoAmI.tsx'
-import { createBrowserRouter } from 'react-router-dom'
+// import WhoAmI from './pages/WhoAmI.tsx'
+// import { createBrowserRouter } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path:'/whoami',
-    element: <WhoAmI/>
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path:'/whoami',
+//     element: <WhoAmI/>
+//   }
+// ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
